8c7b057654cc871746b3ecd55c93f049493d21e6
Sreekanth <sreekanth@wizlearn.com>
2023-08-26 15:25:49 +0800

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.AspNetCore.Authentication.Certificate;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,12 +13,19 @@ builder.Services.AddDbContext<DataContext>(opt =>
     object value = opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
+builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate("./ssl/localhost.pem");
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//Configure the HTTP request pipeline.
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
